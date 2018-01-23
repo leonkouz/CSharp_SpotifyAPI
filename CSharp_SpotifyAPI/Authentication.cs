a350cb0b5146da55354e3776ebe8ab5a43d0bb6b
Leon <leon.kouz@hotmail.com>
2018-01-23 17:26:22 +1100

    Add project files.

---
+++ CSharp_SpotifyAPI/Authentication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_SpotifyAPI
+{
+    class Authentication
+    {
+        //Create a HTTP server to handle responses       
+        //HTTPServer myServer = new HTTPServer(@"C:\Test", 8888);
+
+        //https://developer.spotify.com/web-api/authorization-guide/#implicit_grant_flow
+
+        string urlToAuthenticateWith = "https://accounts.spotify.com/authorize?client_id=305dbadf23cd4d9688868eb01857b54b&redirect_uri=http%3A%2F%2Flocalhost%3A62177&scope=user-read-private%20user-read-email&response_type=token&state=123";
+
+    }
+}
---
+++ TestApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp_SpotifyAPI;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create server with auto assigned port
+            SimpleHttpServer myServer = new SimpleHttpServer(62177, AuthType.Implicit);
+            myServer.Listen();
+
+            Console.WriteLine("Listening on 62177");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
